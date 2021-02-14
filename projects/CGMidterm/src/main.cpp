d78e86b61975742bf412513fcea5141f810c4601
Alexander Chow <alexander.chow@ontariotechu.net>
2021-02-14 18:05:28 -0500

    Scene done

--- projects/CGMidterm/src/main.cpp
+++ projects/CGMidterm/src/main.cpp
@@ -58,7 +58,7 @@ int main() {
 
 		glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 5.0f);
 		glm::vec3 lightCol = glm::vec3(0.9f, 0.85f, 0.5f);
-		float     lightAmbientPow = 0.1f;
+		float     lightAmbientPow = 0.5f;
 		float     lightSpecularPow = 1.0f;
 		glm::vec3 ambientCol = glm::vec3(1.0f);
 		float     ambientPow = 0.1;
@@ -292,7 +292,7 @@ int main() {
 
 		// GL states
 		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CULL_FACE);
+		//glEnable(GL_CULL_FACE);
 		glDepthFunc(GL_LEQUAL); // New 
 
 		#pragma region TEXTURE LOADING
@@ -305,6 +305,11 @@ int main() {
 		Texture2D::sptr box = Texture2D::LoadFromFile("images/box.bmp");
 		Texture2D::sptr boxSpec = Texture2D::LoadFromFile("images/box-reflections.bmp");
 		Texture2D::sptr simpleFlora = Texture2D::LoadFromFile("images/SimpleFlora.png");
+		Texture2D::sptr Table = Texture2D::LoadFromFile("images/Table.png");
+		Texture2D::sptr Fence = Texture2D::LoadFromFile("images/Diffuse.jpg");
+		Texture2D::sptr White = Texture2D::LoadFromFile("images/White.png");
+		Texture2D::sptr ballwhite = Texture2D::LoadFromFile("images/10536_soccerball_V1_diffuse.jpg");
+		Texture2D::sptr gazebo = Texture2D::LoadFromFile("images/10073_Gazebo_V2_Diffuse.jpg");
 		LUT3D testCube("cubes/BrightenedCorrection.cube");
 
 		// Load the cube map
@@ -339,66 +344,164 @@ int main() {
 			scene->Registry().group<RendererComponent>(entt::get_t<Transform>());
 
 		// Create a material and set some properties for it
-		ShaderMaterial::sptr stoneMat = ShaderMaterial::Create();  
-		stoneMat->Shader = shader;
-		stoneMat->Set("s_Diffuse", stone);
-		stoneMat->Set("s_Specular", stoneSpec);
-		stoneMat->Set("u_Shininess", 2.0f);
-		stoneMat->Set("u_TextureMix", 0.0f); 
+		ShaderMaterial::sptr GrassMat = ShaderMaterial::Create();  
+		GrassMat->Shader = shader;
+		GrassMat->Set("s_Diffuse", grass);
+		GrassMat->Set("s_Specular", stoneSpec);
+		GrassMat->Set("u_Shininess", 2.0f);
+		GrassMat->Set("u_TextureMix", 0.0f);
+		
+		ShaderMaterial::sptr FenceMat = ShaderMaterial::Create();  
+		FenceMat->Shader = shader;
+		FenceMat->Set("s_Diffuse", Fence);
+		FenceMat->Set("s_Specular", stoneSpec);
+		FenceMat->Set("u_Shininess", 2.0f);
+		FenceMat->Set("u_TextureMix", 0.0f); 
 
-		ShaderMaterial::sptr grassMat = ShaderMaterial::Create();
-		grassMat->Shader = shader;
-		grassMat->Set("s_Diffuse", grass);
-		grassMat->Set("s_Specular", noSpec);
-		grassMat->Set("u_Shininess", 2.0f);
-		grassMat->Set("u_TextureMix", 0.0f);
+		ShaderMaterial::sptr WhiteMat = ShaderMaterial::Create();
+		WhiteMat->Shader = shader;
+		WhiteMat->Set("s_Diffuse", White);
+		WhiteMat->Set("s_Specular", boxSpec);
+		WhiteMat->Set("u_Shininess", 8.0f);
+		WhiteMat->Set("u_TextureMix", 0.0f);
 
-		ShaderMaterial::sptr boxMat = ShaderMaterial::Create();
-		boxMat->Shader = shader;
-		boxMat->Set("s_Diffuse", box);
-		boxMat->Set("s_Specular", boxSpec);
-		boxMat->Set("u_Shininess", 8.0f);
-		boxMat->Set("u_TextureMix", 0.0f);
+		ShaderMaterial::sptr ballMat = ShaderMaterial::Create();
+		ballMat->Shader = shader;
+		ballMat->Set("s_Diffuse", ballwhite);
+		ballMat->Set("s_Specular", noSpec);
+		ballMat->Set("u_Shininess", 8.0f);
+		ballMat->Set("u_TextureMix", 0.5f);
+		
+		ShaderMaterial::sptr TableMat = ShaderMaterial::Create();
+		TableMat->Shader = shader;
+		TableMat->Set("s_Diffuse", Table);
+		TableMat->Set("s_Specular", noSpec);
+		TableMat->Set("u_Shininess", 8.0f);
+		TableMat->Set("u_TextureMix", 0.0f);
+		
+		ShaderMaterial::sptr GazeboMat = ShaderMaterial::Create();
+		GazeboMat->Shader = shader;
+		GazeboMat->Set("s_Diffuse", gazebo);
+		GazeboMat->Set("s_Specular", noSpec);
+		GazeboMat->Set("u_Shininess", 8.0f);
+		GazeboMat->Set("u_TextureMix", 0.0f);
 
-		ShaderMaterial::sptr simpleFloraMat = ShaderMaterial::Create();
-		simpleFloraMat->Shader = shader;
-		simpleFloraMat->Set("s_Diffuse", simpleFlora);
-		simpleFloraMat->Set("s_Specular", noSpec);
-		simpleFloraMat->Set("u_Shininess", 8.0f);
-		simpleFloraMat->Set("u_TextureMix", 0.0f);
+		GameObject obj1 = scene->CreateEntity("Table");
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/Table.obj");
+			obj1.emplace<RendererComponent>().SetMesh(vao).SetMaterial(TableMat);
+			obj1.get<Transform>().SetLocalPosition(0.0f, 0.0f, 0.0f);
+			obj1.get<Transform>().SetLocalRotation(90.0f, 0.0f, 0.0f);
+			obj1.get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
+			BehaviourBinding::BindDisabled<SimpleMoveBehaviour>(obj1);
+		}
 
-		GameObject obj1 = scene->CreateEntity("Ground"); 
+		GameObject obj2 = scene->CreateEntity("Plane");
 		{
 			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/plane.obj");
-			obj1.emplace<RendererComponent>().SetMesh(vao).SetMaterial(grassMat);
-		}
-
-		GameObject obj2 = scene->CreateEntity("monkey_quads");
-		{
-			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/monkey_quads.obj");
-			obj2.emplace<RendererComponent>().SetMesh(vao).SetMaterial(stoneMat);
-			obj2.get<Transform>().SetLocalPosition(0.0f, 0.0f, 2.0f);
-			obj2.get<Transform>().SetLocalRotation(0.0f, 0.0f, -90.0f);
+			obj2.emplace<RendererComponent>().SetMesh(vao).SetMaterial(GrassMat);
+			obj2.get<Transform>().SetLocalPosition(0.0f, 0.0f, 0.0f);
+			obj2.get<Transform>().SetLocalRotation(00.0f, 0.0f, -90.0f);
+			obj2.get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
 			BehaviourBinding::BindDisabled<SimpleMoveBehaviour>(obj2);
 		}
+		
+		std::vector<GameObject> fenceBack;
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/fence.obj");
+			for (int i = 0; i < 5; i++)
+			{
+				fenceBack.push_back(scene->CreateEntity("fence back" + (std::to_string(i + 1))));
+				fenceBack[i].emplace<RendererComponent>().SetMesh(vao).SetMaterial(FenceMat);
+				fenceBack[i].get<Transform>().SetLocalPosition(-16.0f + i * 8, 20.0f, 2.0f);
+				fenceBack[i].get<Transform>().SetLocalRotation(90.0f, 0.0f, 0.0f);
+				fenceBack[i].get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
+			}
+		}
+		
+		std::vector<GameObject> fenceRight;
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/fence.obj");
+			for (int i = 0; i < 5; i++)
+			{
+				fenceRight.push_back(scene->CreateEntity("fence right" + (std::to_string(i + 1))));
+				fenceRight[i].emplace<RendererComponent>().SetMesh(vao).SetMaterial(FenceMat);
+				fenceRight[i].get<Transform>().SetLocalPosition(20, -16.0f + i * 8, 2.0f);
+				fenceRight[i].get<Transform>().SetLocalRotation(90.0f, 0.0f, 90.0f);
+				fenceRight[i].get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
+			}
+		}
+		
+		std::vector<GameObject> fenceLeft;
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/fence.obj");
+			for (int i = 0; i < 5; i++)
+			{
+				fenceLeft.push_back(scene->CreateEntity("fence left" + (std::to_string(i + 1))));
+				fenceLeft[i].emplace<RendererComponent>().SetMesh(vao).SetMaterial(FenceMat);
+				fenceLeft[i].get<Transform>().SetLocalPosition(-20, -15.0f + i * 8, 2.0f);
+				fenceLeft[i].get<Transform>().SetLocalRotation(90.0f, 0.0f, 90.0f);
+				fenceLeft[i].get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
+			}
+		}
+		
+		std::vector<GameObject> fenceUp;
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/fence.obj");
+			for (int i = 0; i < 6; i++)
+			{
+				fenceUp.push_back(scene->CreateEntity("fence Up" + (std::to_string(i + 1))));
+				fenceUp[i].emplace<RendererComponent>().SetMesh(vao).SetMaterial(FenceMat);
+				fenceUp[i].get<Transform>().SetLocalPosition(-17.0f + i * 8, -20, 2.0f);
+				fenceUp[i].get<Transform>().SetLocalRotation(90.0f, 0.0f, 0.0f);
+				fenceUp[i].get<Transform>().SetLocalScale(1.0f, 1.0f, 1.0f);
+			}
+		}
 
-		std::vector<glm::vec2> allAvoidAreasFrom = { glm::vec2(-4.0f, -4.0f) };
-		std::vector<glm::vec2> allAvoidAreasTo = { glm::vec2(4.0f, 4.0f) };
+		GameObject obj7 = scene->CreateEntity("goal");
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/football goal.obj");
+			obj7.emplace<RendererComponent>().SetMesh(vao).SetMaterial(WhiteMat);
+			obj7.get<Transform>().SetLocalPosition(12.0f, -15.0f, 0.0f);
+			obj7.get<Transform>().SetLocalRotation(90.0f, 0.0f, 90.0f);
+			obj7.get<Transform>().SetLocalScale(0.02f, 0.02f, 0.02f);
+		}
+		
+		GameObject obj8 = scene->CreateEntity("goal2");
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/football goal.obj");
+			obj8.emplace<RendererComponent>().SetMesh(vao).SetMaterial(WhiteMat);
+			obj8.get<Transform>().SetLocalPosition(-12.0f, -5.0f, 0.0f);
+			obj8.get<Transform>().SetLocalRotation(90.0f, 0.0f, -90.0f);
+			obj8.get<Transform>().SetLocalScale(0.02f, 0.02f, 0.02f);
+		}
+		
+		GameObject obj9 = scene->CreateEntity("Ball");
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/ball.obj");
+			obj9.emplace<RendererComponent>().SetMesh(vao).SetMaterial(ballMat);
+			obj9.get<Transform>().SetLocalPosition(0.0f, -8.0f, 0.0f);
+			obj9.get<Transform>().SetLocalRotation(0.0f, 0.0f, 0.0f);
+			obj9.get<Transform>().SetLocalScale(0.07f, 0.07f, 0.07f);
 
-		std::vector<glm::vec2> rockAvoidAreasFrom = { glm::vec2(-3.0f, -3.0f), glm::vec2(-19.0f, -19.0f), glm::vec2(5.0f, -19.0f),
-														glm::vec2(-19.0f, 5.0f), glm::vec2(-19.0f, -19.0f) };
-		std::vector<glm::vec2> rockAvoidAreasTo = { glm::vec2(3.0f, 3.0f), glm::vec2(19.0f, -5.0f), glm::vec2(19.0f, 19.0f),
-														glm::vec2(19.0f, 19.0f), glm::vec2(-5.0f, 19.0f) };
-		glm::vec2 spawnFromHere = glm::vec2(-19.0f, -19.0f);
-		glm::vec2 spawnToHere = glm::vec2(19.0f, 19.0f);
-
-		EnvironmentGenerator::AddObjectToGeneration("models/simplePine.obj", simpleFloraMat, 150,
-			spawnFromHere, spawnToHere, allAvoidAreasFrom, allAvoidAreasTo);
-		EnvironmentGenerator::AddObjectToGeneration("models/simpleTree.obj", simpleFloraMat, 150,
-			spawnFromHere, spawnToHere, allAvoidAreasFrom, allAvoidAreasTo);
-		EnvironmentGenerator::AddObjectToGeneration("models/simpleRock.obj", simpleFloraMat, 40,
-			spawnFromHere, spawnToHere, rockAvoidAreasFrom, rockAvoidAreasTo);
-		EnvironmentGenerator::GenerateEnvironment();
+			// Bind returns a smart pointer to the behaviour that was added
+			auto pathing = BehaviourBinding::Bind<FollowPathBehaviour>(obj9);
+			// Set up a path for the object to follow
+			pathing->Points.push_back({ -5.0f, -10.0f, 0.0f });
+			pathing->Points.push_back({ 5.0f, -10.0f, 0.0f });
+			pathing->Points.push_back({ 5.0f, -8.0f, 0.0f });
+			pathing->Points.push_back({ -5.0f, -8.0f, 0.0f });
+			pathing->Speed = 4.0f;
+		}
+		
+		GameObject obj10 = scene->CreateEntity("Gazebo");
+		{
+			VertexArrayObject::sptr vao = ObjLoader::LoadFromFile("models/10073_Gazebo_V2_L3.obj");
+			obj10.emplace<RendererComponent>().SetMesh(vao).SetMaterial(GazeboMat);
+			obj10.get<Transform>().SetLocalPosition(0.0f, 12.0f, 0.0f);
+			obj10.get<Transform>().SetLocalRotation(0.0f, 0.0f, 0.0f);
+			obj10.get<Transform>().SetLocalScale(0.02f, 0.02f, 0.02f);
+		}
 
 		// Create an object to be our camera
 		GameObject cameraObject = scene->CreateEntity("Camera");
